221aea1c8bd671fd2e6fb4ccb883d623bead3cd6
hamza bejaoui <hamza.bejaoui@wevioo.com>
2019-06-10 16:10:13 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+    apiKey: "AIzaSyACr-JQDVhtcA7oigoIm2HpeX_DsxSpiAY",
+    authDomain: "m-city-d5763.firebaseapp.com",
+    databaseURL: "https://m-city-d5763.firebaseio.com",
+    projectId: "m-city-d5763",
+    storageBucket: "m-city-d5763.appspot.com",
+    messagingSenderId: "798250391338",
+    appId: "1:798250391338:web:8c8d1e0775af0d32"
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches');
+export  {
+    firebase,
+    firebaseMatches
+}
